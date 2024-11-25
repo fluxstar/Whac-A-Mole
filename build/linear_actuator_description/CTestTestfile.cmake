79de15053acc83abf3103f6dccba48c061970a1f
fluxstar <krant115@umn.edu>
2024-11-25 12:45:17 -0600

    Removed build, devel folders

--- build/linear_actuator_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/krant115/whac-a-mole/src/linear_actuator_description
-# Build directory: /home/krant115/whac-a-mole/build/linear_actuator_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/whac-a-mole_description/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/krant115/whac-a-mole/src/whac-a-mole_description
-# Build directory: /home/krant115/whac-a-mole/build/whac-a-mole_description
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
